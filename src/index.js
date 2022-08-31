c662f2ca73a9b5dd47bb20046807f1821fa1f959
依力 <yantaq@bilig.biz>
2022-08-31 08:00:35 +0800

    add duration option

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@
                 density: 100,
                 clarity: 2,
                 zIndex: 1,
+                duration: 1000,
                 supportTip: '您的浏览器不支持Canvas'
             }, params);
             this._init();
@@ -41,7 +42,7 @@
                 if (!document.getElementById(self.params.id + '-canvas')) {
                     self._init();
                 }
-            }, 1000);
+            }, this.params.duration);
         }
 
         _init() {
@@ -68,14 +69,13 @@
         }
 
         _fill() {
-            let redundance = 10;
+            let reDunDance = 10;
             let canvas = document.getElementById(this.params.id + '-canvas');
             let containerNode = document.getElementById(this.params.id);
             let cxt = canvas.getContext('2d');
             let times = containerNode.offsetWidth * this.params.clarity / this.params.density + redundance;
             let heightTimes = containerNode.offsetHeight * this.params.clarity * 1.5 / this.params.density;
             cxt.rotate(-15 * Math.PI / 180);
-
             for (let i = 0; i < times; i++) {
                 for (let j = 0; j < heightTimes; j++) {
                     cxt.fillStyle = this.params.color;
@@ -84,7 +84,7 @@
                         + this.params.fontWeight + ' '
                         + this.params.fontSize + ' '
                         + this.params.fontFamily + ' ';
-                    cxt.fillText(this.params.text, this.params.density * (i - redundance / 2), j * this.params.density);
+                    cxt.fillText(this.params.text, this.params.density * (i - reDunDance / 2), j * this.params.density);
                 }
             }
         }
